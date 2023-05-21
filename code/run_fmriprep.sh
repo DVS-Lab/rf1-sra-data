7396c6f779fb8bf3a6de4eceae4037cf00b37972
Jimmy Wyngaarden <tun46412@cla18994.tu.temple.edu>
2023-05-21 07:44:07 -0400

    update for remaining sub list

--- code/run_fmriprep.sh
+++ code/run_fmriprep.sh
@@ -5,13 +5,13 @@ scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 maindir="$(dirname "$scriptdir")"
 bidsdir=$maindir/bids
 
-#for sub  in $bidsdir/sub*; do
-for sub in 'sub-10589' 'sub-10663'; do
+#for sub in $bidsdir/sub*; do
+for sub in 'sub-10555' 'sub-10584' 'sub-10596' 'sub-10603' 'sub-10608' 'sub-10617' 'sub-10656' 'sub-10663' 'sub-10677'; do
 
 	sub="${sub##*/}"
 
 	script=${scriptdir}/fmriprep.sh
-	NCORES=1
+	NCORES=2
 	while [ $(ps -ef | grep -v grep | grep $script | wc -l) -ge $NCORES ]; do
 		sleep 1s
 	done
