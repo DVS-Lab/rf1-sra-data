61077daadaab29e07823129304cafe942304acef
David V. Smith <david.v.smith@temple.edu>
2023-07-29 14:33:40 -0400

    clean up and make consistent

--- code/run_fmriprep.sh
+++ code/run_fmriprep.sh
@@ -2,23 +2,16 @@
 
 # ensure paths are correct irrespective from where user runs the script
 scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-maindir="$(dirname "$scriptdir")"
-bidsdir=$maindir/bids
 
-#for sub in $bidsdir/sub*; do
-# CHECK 10585 & 10589 OUTPUT, MISSING .HTML FILES?
-#for sub in 'sub-10555' 'sub-10584' 'sub-10596' 'sub-10603' 'sub-10608' 'sub-10617' 'sub-10656' 'sub-10663' 'sub-10677'; do
-for sub in 'sub-10584'; do
-
-	sub="${sub##*/}"
+for sub in `cat ${scriptdir}/newsubs.txt` ; do
 
 	script=${scriptdir}/fmriprep.sh
-	NCORES=2
+	NCORES=4
 	while [ $(ps -ef | grep -v grep | grep $script | wc -l) -ge $NCORES ]; do
-		sleep 1s
+		sleep 5s
 	done
-	echo $script $sub
 	bash $script $sub &
 	sleep 5s
+	
 done
-#python ${scriptdir}/MakeConfounds.py --fmriprepDir "${scriptdir}/../derivatives/fmriprep
+
--- code/run_prepdata.sh
+++ code/run_prepdata.sh
@@ -2,31 +2,15 @@
 
 # ensure paths are correct irrespective from where user runs the script
 scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-maindir="$(dirname "$scriptdir")"
-
-sourcedir=/ZPOOL/data/sourcedata/sourcedata/rf1-sra/*
-DLscript=${scriptdir}/downloadXNAT.py
-
-python $DLscript
 
 for sub in `cat ${scriptdir}/newsubs.txt` ; do
-#for sub in 10701 10691 10674 10649 10317; do
+
 	script=${scriptdir}/prepdata.sh
 	NCORES=12
 	while [ $(ps -ef | grep -v grep | grep $script | wc -l) -ge $NCORES ]; do
-		echo "made it here" $script $sub
-		sleep 1m
+		sleep 5s
 	done
-        echo "Running prepdata" $script $sub
-	bash $script $sub &
+   bash $script $sub &
 	sleep 5s
-
+	
 done
-
-NCORES=8
-	while [ $(ps -ef | grep -v grep | grep $script | wc -l) -ge $NCORES ]; do
-		sleep 1s
-	done
-#bash ${scriptdir}/run_motioneval.sh
-#python ${scriptdir}/IDoutliers.py --mriscDir "${sourcedir}/derivatives/mriqc"
-#bash ${scriptdir}/run_gen3colfiles.sh
