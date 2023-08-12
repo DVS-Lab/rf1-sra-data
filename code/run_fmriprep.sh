ab4626c727e190de6a8c2c5a5526edace818942f
David V. Smith <david.v.smith@temple.edu>
2023-08-12 15:53:20 -0400

    ignore bids and derivatives

--- code/run_fmriprep.sh
+++ code/run_fmriprep.sh
@@ -3,7 +3,8 @@
 # ensure paths are correct irrespective from where user runs the script
 scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-for sub in `cat ${scriptdir}/newsubs.txt` ; do
+#for sub in `cat ${scriptdir}/newsubs.txt` ; do
+for sub in 10317; do
 
 	script=${scriptdir}/fmriprep.sh
 	NCORES=2 # need to do on OwlsNest with datalad since each sub has 8 runs of data with 4 echoes (needs 32 processors per sub)
