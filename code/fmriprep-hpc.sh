ffd8a6cc7d4989c694713f728619e9acdedc1776
DVSneuro <david.v.smith@temple.edu>
2023-09-15 22:22:47 -0400

    submit arrays

--- code/fmriprep-hpc.sh
+++ code/fmriprep-hpc.sh
@@ -2,7 +2,7 @@
 #PBS -l walltime=24:00:00
 #PBS -N fmriprep
 #PBS -q normal
-#PBS -l nodes=1:ppn=28
+#PBS -l nodes=12:ppn=4
 
 # load modules and go to workdir
 module load fsl/6.0.2
@@ -11,13 +11,13 @@ module load singularity/3.8.5
 cd $PBS_O_WORKDIR
 
 # ensure paths are correct
-maindir=~/work/rf1-sra-data #this should be the only line that has to change if the rest of the script is set up correctly
+projectname=rf1-sra-data #this should be the only line that has to change if the rest of the script is set up correctly
+maindir=~/work/$projectname
 scriptdir=$maindir/code
 bidsdir=$maindir/bids
 logdir=$maindir/logs
 mkdir -p $logdir
 
-sub=$1
 
 rm -f $logdir/cmd_fmriprep_${PBS_JOBID}.txt
 touch $logdir/cmd_fmriprep_${PBS_JOBID}.txt
@@ -28,7 +28,7 @@ if [ ! -d $maindir/derivatives ]; then
 	mkdir -p $maindir/derivatives
 fi
 
-scratchdir=~/scratch/fmriprep
+scratchdir=~/scratch/$projectname/fmriprep
 if [ ! -d $scratchdir ]; then
 	mkdir -p $scratchdir
 fi
@@ -38,42 +38,44 @@ MPLCONFIGDIR_DIR=~/work/mplconfigdir
 export SINGULARITYENV_TEMPLATEFLOW_HOME=/opt/templateflow
 export SINGULARITYENV_MPLCONFIGDIR=/opt/mplconfigdir
 
-# check this list and update intendedfor to make fmpas match
-if [ $sub -eq 10317 ] || [ $sub -eq 10369 ] || [ $sub -eq 10402 ] || [ $sub -eq 10486 ] || [ $sub -eq 10541 ] || [ $sub -eq 10572 ] || [ $sub -eq 10584 ] || [ $sub -eq 10589 ] || [ $sub -eq 10691 ] || [ $sub -eq 10701 ]; then
-	echo singularity run --cleanenv \
-	-B ${TEMPLATEFLOW_DIR}:/opt/templateflow \
-	-B ${MPLCONFIGDIR_DIR}:/opt/mplconfigdir \
-	-B $maindir:/base \
-	-B ~/work/tools/licenses:/opts \
-	-B $scratchdir:/scratch \
-	~/work/tools/fmriprep-23.1.3.simg \
-	/base/bids /base/derivatives/fmriprep \
-	participant --participant_label $sub \
-	--stop-on-first-crash \
-	--nthreads 12 \
-	--me-output-echos \
-	--use-syn-sdc \
-	--cifti-output 91k \
-	--output-spaces fsLR fsaverage MNI152NLin6Asym \
-	--bids-filter-file /base/code/fmriprep_config.json \
-	--fs-license-file /opts/fs_license.txt -w /scratch >> $logdir/cmd_fmriprep_${PBS_JOBID}.txt
-else
-	echo singularity run --cleanenv \
-	-B ${TEMPLATEFLOW_DIR}:/opt/templateflow \
-	-B ${MPLCONFIGDIR_DIR}:/opt/mplconfigdir \
-	-B $maindir:/base \
-	-B ~/work/tools/licenses:/opts \
-	-B $scratchdir:/scratch \
-	~/work/tools/fmriprep-23.1.3.simg \
-	/base/bids /base/derivatives/fmriprep \
-	participant --participant_label $sub \
-	--stop-on-first-crash \
-	--nthreads 12 \
-	--me-output-echos \
-	--cifti-output 91k \
-	--output-spaces fsLR fsaverage MNI152NLin6Asym \
-	--bids-filter-file /base/code/fmriprep_config.json \
-	--fs-license-file /opts/fs_license.txt -w /scratch >> $logdir/cmd_fmriprep_${PBS_JOBID}.txt
-fi
-
+for sub in ${subjects[@]}; do
+	# check this list and update intendedfor to make fmaps match
+	if [ $sub -eq 10317 ] || [ $sub -eq 10369 ] || [ $sub -eq 10402 ] || [ $sub -eq 10486 ] || [ $sub -eq 10541 ] || [ $sub -eq 10572 ] || [ $sub -eq 10584 ] || [ $sub -eq 10589 ] || [ $sub -eq 10691 ] || [ $sub -eq 10701 ]; then
+		echo singularity run --cleanenv \
+		-B ${TEMPLATEFLOW_DIR}:/opt/templateflow \
+		-B ${MPLCONFIGDIR_DIR}:/opt/mplconfigdir \
+		-B $maindir:/base \
+		-B ~/work/tools/licenses:/opts \
+		-B $scratchdir:/scratch \
+		~/work/tools/fmriprep-23.1.3.simg \
+		/base/bids /base/derivatives/fmriprep \
+		participant --participant_label $sub \
+		--stop-on-first-crash \
+		--nthreads 12 \
+		--me-output-echos \
+		--ignore fieldmaps \
+		--use-syn-sdc \
+		--cifti-output 91k \
+		--output-spaces fsLR fsaverage MNI152NLin6Asym \
+		--bids-filter-file /base/code/fmriprep_config.json \
+		--fs-license-file /opts/fs_license.txt -w /scratch >> $logdir/cmd_fmriprep_${PBS_JOBID}.txt
+	else
+		echo singularity run --cleanenv \
+		-B ${TEMPLATEFLOW_DIR}:/opt/templateflow \
+		-B ${MPLCONFIGDIR_DIR}:/opt/mplconfigdir \
+		-B $maindir:/base \
+		-B ~/work/tools/licenses:/opts \
+		-B $scratchdir:/scratch \
+		~/work/tools/fmriprep-23.1.3.simg \
+		/base/bids /base/derivatives/fmriprep \
+		participant --participant_label $sub \
+		--stop-on-first-crash \
+		--nthreads 12 \
+		--me-output-echos \
+		--cifti-output 91k \
+		--output-spaces fsLR fsaverage MNI152NLin6Asym \
+		--bids-filter-file /base/code/fmriprep_config.json \
+		--fs-license-file /opts/fs_license.txt -w /scratch >> $logdir/cmd_fmriprep_${PBS_JOBID}.txt
+	fi
+done
 torque-launch -p $logdir/chk_fmriprep_${PBS_JOBID}.txt $logdir/cmd_fmriprep_${PBS_JOBID}.txt
--- code/run_fmriprep-hpc.sh
+++ code/run_fmriprep-hpc.sh
@@ -4,6 +4,15 @@
 maindir=~/work/rf1-sra-data #this should be the only line that has to change if the rest of the script is set up correctly
 scriptdir=$maindir/code
 
-for sub in `cat ${scriptdir}/newsubs_rf1-sra-data.txt` ; do
-	qsub -F $sub fmriprep-hpc.sh
+
+mapfile -t myArray < ${scriptdir}/newsubs_rf1-sra-data.txt
+
+
+# grab the first 10 elements
+ntasks=10
+counter=0
+while [ $counter -lt ${#myArray[@]} ]; do
+	subjects=${myArray[@]:$counter:$ntasks}
+	let counter=$counter+$ntasks
+	qsub -v subjects=("${subjects[@]}") fmriprep-hpc.sh
 done
