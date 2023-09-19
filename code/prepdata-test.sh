8dfe2f4490e83e8f35224ce9fc366e0decd419fd
DVSneuro <david.v.smith@temple.edu>
2023-09-19 11:25:52 -0400

    fix path to be scriptdir

--- code/prepdata-test.sh
+++ code/prepdata-test.sh
@@ -23,7 +23,7 @@ done
 
 
 # ensure paths are correct irrespective from where user runs the script
-codedir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 dsroot="$(dirname "$scriptdir")"
 
 echo ${dsroot}
