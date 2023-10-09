2af2338b00039b58f6ff29c11a230cc339d7c288
jameswyngaarden <james.wyngaarden@temple.edu>
2023-10-09 15:46:23 -0400

    run new subs

--- code/run_prepdata.sh
+++ code/run_prepdata.sh
@@ -3,8 +3,8 @@
 # ensure paths are correct irrespective from where user runs the script
 scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-for sub in `cat ${scriptdir}/newsubs_rf1-sra-data.txt` ; do
-#for sub in 10777; do
+#for sub in `cat ${scriptdir}/newsubs_rf1-sra-data.txt` ; do
+for sub in 10559 10661 10668 10713 10767 10785 10794 10802 10803 10806 10812; do
 
 	script=${scriptdir}/prepdata.sh
 	NCORES=16
