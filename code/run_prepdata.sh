961c92211055bb52980a6751d36bd1bcfc7e4b9b
jameswyngaarden <james.wyngaarden@temple.edu>
2023-10-18 08:02:20 -0400

    prepdata new subs

--- code/run_prepdata.sh
+++ code/run_prepdata.sh
@@ -4,7 +4,7 @@
 scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 #for sub in `cat ${scriptdir}/newsubs_rf1-sra-data.txt` ; do
-for sub in 10559 10661 10668 10713 10767 10785 10794 10802 10803 10806 10812; do
+for sub in 10559 10661 10668 10713 10748 10767 10785 10794 10801 10802 10803 10806 10812; do
 
 	script=${scriptdir}/prepdata.sh
 	NCORES=16
