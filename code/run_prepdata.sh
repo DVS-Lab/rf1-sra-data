28a1e967bd077e61df5910efa4bedca601e7e13f
jameswyngaarden <james.wyngaarden@temple.edu>
2023-10-27 14:34:39 -0400

    re-run subs missing UGR

--- code/run_prepdata.sh
+++ code/run_prepdata.sh
@@ -5,7 +5,7 @@ scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 #for sub in `cat ${scriptdir}/newsubs_rf1-sra-data.txt` ; do
 #for sub in 10640 10718 10770 10801 10807 10809; do
-for sub in 10713; do
+for sub in 10812 10806 10803 10802 10794 10785 10767; do
 
 	script=${scriptdir}/prepdata.sh
 	NCORES=16
