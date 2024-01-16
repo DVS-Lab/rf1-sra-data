73b4a70cedb6dbcf4d7d79acdf37dd273d8f9bdc
Cooper Joseph Sharp <tun31934@temple.edu>
2024-01-16 11:22:39 -0500

    Updating for cluster use

--- code/run_prepdata.sh
+++ code/run_prepdata.sh
@@ -5,10 +5,11 @@ scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 #for sub in `cat ${scriptdir}/newsubs_rf1-sra-data.txt` ; do
 #for sub in 10640 10718 10770 10801 10807 10809; do
-for sub in 10812 10806 10803 10802 10794 10785 10767; do
+#for sub in 10812 10806 10803 10802 10794 10785 10767; do
+for sub in 10817 10827 10834 10836 10838 10843 10849 10850 10854 10857 10860 10862 10863 10866 ; do
 
 	script=${scriptdir}/prepdata.sh
-	NCORES=16
+	NCORES=30
 	while [ $(ps -ef | grep -v grep | grep $script | wc -l) -ge $NCORES ]; do
 		sleep 5s
 	done
