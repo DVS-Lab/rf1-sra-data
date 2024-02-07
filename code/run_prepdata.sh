abedc775e3ba16032823f9bd06783987959dc1ca
jameswyngaarden <james.wyngaarden@temple.edu>
2024-02-07 14:06:37 -0500

    update analyses

--- code/run_prepdata.sh
+++ code/run_prepdata.sh
@@ -6,7 +6,7 @@ scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 #for sub in `cat ${scriptdir}/newsubs_rf1-sra-data.txt` ; do
 #for sub in 10640 10718 10770 10801 10807 10809; do
 #for sub in 10812 10806 10803 10802 10794 10785 10767; do
-for sub in 10559 10661 ; do
+for sub in 10668 ; do
 
 	script=${scriptdir}/prepdata.sh
 	NCORES=30
---
+++ code/transferbids-hpc.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Ensure paths are correct irrespective from where user runs the script
+scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+basedir="$(dirname "$scriptdir")"
+
+source_directory="/ZPOOL/data/projects/rf1-sra-data/bids/"
+destination_server="@owlsnest.hpc.temple.edu"
+destination_path=":work/rf1-sra-data/bids/"
+
+read -p "Enter AccessNet ID: " destination_user
+
+for sub in `cat ${basedir}/code/sublist_all.txt`; do
+	source_file="$source_directory/sub-$sub"
+	rsync -avh --no-compress --progress "$source_file" "$destination_user""$destination_server""$destination_path"
+done
+
+exit   
