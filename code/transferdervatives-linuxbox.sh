514df589f0f75a4552de5dc654bf8318e8b55577
cooper-sharp <tun31934@temple.edu>
2024-02-14 16:23:07 -0500

    Update transferdervatives-linuxbox.sh

--- code/transferdervatives-linuxbox.sh
+++ code/transferdervatives-linuxbox.sh
@@ -4,11 +4,11 @@
 scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 basedir="$(dirname "$scriptdir")"
 
-source_directory="/home/$destination_user/work/rf1-sra-data/derivatives/fmriprep"
+read -p "Enter AccessNet ID: " destination_user
+
+source_directory="/home/${destination_user}/work/rf1-sra-data/derivatives/fmriprep"
 destination_server="@cla18994.tu.temple.edu"
 destination_path="/ZPOOL/data/projects/rf1-sra-data/derivatives/fmriprep"
-
-read -p "Enter AccessNet ID: " destination_user
 
 for sub in `cat ${basedir}/code/sublist_new.txt`; do
 	source_file="$source_directory/sub-$sub"
