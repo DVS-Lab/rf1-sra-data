d58c2edf03a32485d39269055c66a65d9675afdb
jameswyngaarden <james.wyngaarden@temple.edu>
2024-02-27 12:01:11 -0500

    update newsubs

--- code/run_prepdata.sh
+++ code/run_prepdata.sh
@@ -3,10 +3,10 @@
 # ensure paths are correct irrespective from where user runs the script
 scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-#for sub in `cat ${scriptdir}/newsubs_rf1-sra-data.txt` ; do
+for sub in `cat ${scriptdir}/sublist_new.txt` ; do
 #for sub in 10640 10718 10770 10801 10807 10809; do
 #for sub in 10812 10806 10803 10802 10794 10785 10767; do
-for sub in 10896 10908; do
+#for sub in ; do
 
 	script=${scriptdir}/prepdata.sh
 	NCORES=20
