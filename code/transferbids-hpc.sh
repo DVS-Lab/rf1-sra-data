d062cd76935797c24b4dd00dff648660bc97bf57
Sharp <tun31934@cla18994.tu.temple.edu>
2024-04-16 12:37:16 -0400

    Update scripts to run 10929 after downloadXNAT.py error

--- code/transferbids-hpc.sh
+++ code/transferbids-hpc.sh
@@ -10,7 +10,8 @@ destination_path=":work/rf1-sra-data/bids/"
 
 read -p "Enter AccessNet ID: " destination_user
 
-for sub in `cat ${basedir}/code/sublist_new.txt`; do
+#for sub in `cat ${basedir}/code/sublist_new.txt`; do
+for sub in 10929; do
 	source_file="$source_directory/sub-$sub"
     rsync -avh --no-compress --progress --include='*/' --include='*.nii.gz' --include='*.json' --exclude='*' "${source_file}" "${destination_user}${destination_server}${destination_path}"
 done
