99cdc010db2cfdf9b1f146708ab576d08e8477ad
Cooper Sharp <102628943+cooper-sharp@users.noreply.github.com>
2024-04-18 12:04:52 -0400

    Update transferbids-hpc.sh with description

--- code/transferbids-hpc.sh
+++ code/transferbids-hpc.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# This script is used to transfer BIDS info from the local to cluster
+
 # Ensure paths are correct irrespective from where user runs the script
 scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 basedir="$(dirname "$scriptdir")"
@@ -10,8 +12,8 @@ destination_path=":work/rf1-sra-data/bids/"
 
 read -p "Enter AccessNet ID: " destination_user
 
-#for sub in `cat ${basedir}/code/sublist_new.txt`; do
-for sub in 10929; do
+for sub in `cat ${basedir}/code/sublist_new.txt`; do
+#for sub in 10929; do
 	source_file="$source_directory/sub-$sub"
     rsync -avh --no-compress --progress --include='*/' --include='*.nii.gz' --include='*.json' --exclude='*' "${source_file}" "${destination_user}${destination_server}${destination_path}"
 done
