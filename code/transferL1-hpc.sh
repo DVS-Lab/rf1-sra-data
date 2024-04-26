9b8967ed87bebe4bf6e3059c76d1c660429e9359
cooper-sharp <tun31934@temple.edu>
2024-04-26 10:59:58 -0400

    Working version of transfer L1

--- code/transferL1-hpc.sh
+++ code/transferL1-hpc.sh
@@ -8,6 +8,7 @@ basedir="$(dirname "$scriptdir")"
 
 
 #bids_directory="/ZPOOL/data/projects/rf1-datapaper-dev/bids"
+fmriprep_dir=/ZPOOL/data/projects/rf1-sra-data/derivatives/fmriprep
 destination_server="@owlsnest.hpc.temple.edu"
 #destination_path=":work/rf1-sra-data/bids"
 
@@ -15,13 +16,17 @@ read -p "Enter AccessNet ID: " destination_user
 	
 for sub in `cat ${scriptdir}/sublist_new-flip.txt` ; do
 
+	source_file="$fmriprep_dir/sub-$sub"
+	
 	# Transfer Preproc BOLD files
-	rsync -avh --no-compress --progress --include='*/' --include='*MNI152NLin6Asym_desc-preproc_bold.nii.gz' --exclude='*' "/ZPOOL/data/projects/rf1-sra-data/derivatives/fmriprep/sub-${sub}/" "${destination_user}${destination_server}:work/rf1-sra-data/derivatives/fmriprep/sub-${sub}/"
+	rsync -avh --no-compress --progress --include='*/' --include='*MNI152NLin6Asym_desc-preproc_bold.nii.gz' --exclude='*' "${source_file}" "${destination_user}${destination_server}:work/rf1-sra-data/derivatives/fmriprep/sub-${sub}/"
+	sleep 5
 
 	# Transfer Confound files
-	#rsync -avh --no-compress --progress --include='*/' --include='*_confounds_timeseries.tsv' --exclude='*' "/ZPOOL/data/projects/rf1-sra-data/derivatives/fmriprep/sub-${sub}/" "${destination_user}${destination_server}:work/rf1-sra-data/derivatives/fmriprep/sub-${sub}/"
+	#rsync -avh --no-compress --progress --include='*/' --include='*_confounds_timeseries.tsv' --exclude='*' "${source_file}" "${destination_user}${destination_server}:work/rf1-sra-data/derivatives/fmriprep/sub-${sub}/"
 
 	# Transfer events.tsv files
 	#rsync -avh --no-compress --progress --include='*/' --include='*_events.tsv' --exclude='*' "${bids_directory}/sub-${sub}/" "${destination_user}${destination_server}${destination_path}/sub-${sub}"
-
 done
+
+exit
